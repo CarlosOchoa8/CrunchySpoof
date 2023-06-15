e312299c480d3ea9631f776db8977b010956edf9
carlos-ochoa8 <carlosochoa@tae.capital>
2023-06-14 21:11:16 -0600

    Login y generacion de token

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshSlidingView
 from .views import RegisterUser
 
 urlpatterns = [
-    path('Auth/UserRegister/', RegisterUser.as_view())
+    path('Auth/UserRegister/', RegisterUser.as_view()),
+    path('auth/login/', TokenObtainPairView.as_view()),
+    path('auth/refresh/', TokenRefreshSlidingView.as_view()),
 ]
